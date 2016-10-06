421c6ff50bc361ae1d8f7c7f2f6940f0128db8c4
mariobp <mariobarrpach@gmail.com>
2016-10-06 13:39:24 -0500

    INterfaz

--- interface/static/js/app.js
+++ interface/static/js/app.js
@@ -1,5 +1,49 @@
-angular.module('App', ['ngMaterial'])
+angular.module('App', ['ngMaterial', 'ngMessages'])
 
-.controller('AppCtrl', function($http, $scope) {
-  $scope.hola = "Hola Mundo";
+.config(function($interpolateProvider) {
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
+})
+
+.controller('AppCtrl', function($scope, $http) {
+    $scope.hola = "Hola Mundo";
+    $scope.placas = [{
+        id: 1,
+        numero: "720WW"
+    }, {
+        id: 2,
+        numero: "541MF"
+    }, {
+        id: 3,
+        numero: "666XX"
+    }, ];
+
+    $scope.selectedId = 2;
+
+    $scope.servicios = [
+      {
+        nombre: "Lavado",
+        id:1
+      },
+      {
+        nombre: "Pulida",
+        id:2
+      },
+      {
+        nombre: "Cambio de aceite",
+        id:3
+      },
+      {
+        nombre: "Lavado",
+        id:4
+      },
+      {
+        nombre: "Pulida",
+        id:5
+      },
+      {
+        nombre: "Cambio de aceite",
+        id:6
+      }
+    ];
 });
