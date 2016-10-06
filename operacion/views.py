30761c0c9ccb654d438678d32a332e5f5abe020b
darkdrei <darkdrei88@gmail.com>
2016-10-06 01:38:56 -0500

    registro vista de servicio de tipos de servicio

--- operacion/views.py
+++ operacion/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from supra import views as supra
+import models
 
-# Create your views here.
+
+class TiposServicios(supra.SupraListView):
+    model = models.TipoServicio
+    list_display = ['id', 'nombre']
+    paginate_by = 100000
+# end class
