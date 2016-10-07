22ca4f382d14faccb8e9534e96cf696e0ad6d171
darkdrei <darkdrei88@gmail.com>
2016-10-07 12:34:23 -0500

    actualizacion

--- cliente/admin.py
+++ cliente/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from exileui.admin import exileui, ExStacked, ExTabular, DateRangeEX
 import nested_admin
 import models
+import forms
 
 
 # Register your models here.
 class VehiculoInline(admin.StackedInline):
     model = models.Vehiculo
     extra = 1
+# end class
+
+
+class VehiculoAdmin(admin.ModelAdmin):
+    list_display = ['placa', 'tipo', 'cliente']
+    list_filter = ['placa', 'tipo', 'cliente']
+    form = forms.AddVehivuloFormAdmin
 # end class
 
 
@@ -18,6 +26,12 @@ class ClienteAdmin(admin.ModelAdmin):
     search_fields = ['nombre', 'apellidos', 'identificacion']
 # end class
 
+
+class TipoVehiculoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'descripcion']
+    list_filter = ['nombre', 'descripcion']
+    list_fileds = ['nombre', 'descripcion']
+# end class
 exileui.register(models.Cliente, ClienteAdmin)
-exileui.register(models.TipoVehiculo)
-exileui.register(models.Vehiculo)
+exileui.register(models.TipoVehiculo, TipoVehiculoAdmin)
+exileui.register(models.Vehiculo, VehiculoAdmin)
