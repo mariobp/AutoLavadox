fc45d2ed319c04efc73ac393f5f1642d81f48bd5
mariobp <mariobarrpach@gmail.com>
2016-10-06 23:01:26 -0500

    Dialog add vehiculo

--- interface/static/js/app.js
+++ interface/static/js/app.js
@@ -77,7 +77,9 @@ angular.module('App', ['ngMaterial', 'ngMessages'])
     $scope.vehiculoActual = function(){
       console.log($scope.selectedItem);
       if ($scope.selectedItem) {
-          $scope.nombre = $scope.selectedItem.nombre + " " + $scope.selectedItem.apellidos;
+					if ($scope.selectedItem.nombre && $scope.selectedItem.apellidos ) {
+						$scope.nombre = $scope.selectedItem.nombre + " " + $scope.selectedItem.apellidos;
+					}
           $scope.identificacion = $scope.selectedItem.cedula;
           $scope.tipo = $scope.selectedItem.tipov;
           if (!$scope.placas.includes($scope.selectedItem)) {
@@ -106,14 +108,85 @@ angular.module('App', ['ngMaterial', 'ngMessages'])
         $mdDialog.show({
           templateUrl: '/template/add/',
           parent: angular.element(document.body),
+					controller: 'DialogController',
           clickOutsideToClose:true,
+					locals: {
+						tipos:$scope.tipos,
+						placa: $scope.search
+					}
+
            // Only for -xs, -sm breakpoints.
         })
         .then(function(answer) {
+					console.log("entro");
           $scope.status = 'You said the information was "' + answer + '".';
-        }, function() {
+        }, function(a) {
+					console.log(a);
+					console.log("entro");
           $scope.status = 'You cancelled the dialog.';
         });
     };
+})
 
+.controller('DialogController', function($scope, $http, $mdDialog, $mdToast, $httpParamSerializer, placa, tipos){
+		$scope.tipos = tipos;
+		$scope.data = {};
+		$scope.data.placa = placa;
+		var formData = new FormData();
+
+		$scope.closeDialog = function() {
+			  $mdDialog.hide();
+		};
+
+		$scope.enviar = function(){
+			formData.append('placa', $scope.data.placa);
+			formData.append('tipo', $scope.data.tipo);
+			$http({
+				url:'/cliente/add/vehiculo/',
+				method: 'POST',
+				data: $httpParamSerializer($scope.data),
+			  headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+				},
+			}).then(function doneCallbacks(response){
+					console.log(response);
+					$mdDialog.hide();
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('Guardado Exitoso')
+			        .hideDelay(3000)
+					);
+			}, function failCallbacks(response){
+					console.log(response);
+					if (response.status == 400) {
+						console.log("400");
+						console.log(response.data.placa);
+						if (response.data.placa) {
+							$mdToast.show(
+								$mdToast.simple()
+									.textContent("Placa: " + response.data.placa[0])
+									.position("bottom")
+					        .hideDelay(3000)
+							);
+						}else if(response.data.tipo){
+							$mdToast.show(
+								$mdToast.simple()
+									.textContent("Tipo: " + response.data.tipo[0])
+									.position("bottom")
+					        .hideDelay(3000)
+							);
+						}
+					}else if (response.status == 500) {
+						$mdDialog.show(
+							$mdDialog.alert()
+								.parent(angular.element(document.querySelector('#popupContainer')))
+								.clickOutsideToClose(true)
+								.title('Error del servidor')
+								.textContent('Hay un error, contacte a el administrador.')
+								.ariaLabel('Alert Dialog Error')
+								.ok('OK')
+						);
+					}
+			});
+		};
 });
