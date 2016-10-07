21bb528dfd26007862ae68518b32337035ee930e
darkdrei <darkdrei88@gmail.com>
2016-10-06 20:27:01 -0500

    servicio de mostrar trabajadores

--- empleados/urls.py
+++ empleados/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^login/', views.Login.as_view(), name='user-login'),
-    url(r'^logout/', views.Logout.as_view(), name='user-logout'),
+    url(r'^login/$', views.Login.as_view(), name='user-login'),
+    url(r'^logout/$', views.Logout.as_view(), name='user-logout'),
+]
+
+# servicio que retorna los Operarios
+urlpatterns = [
+    url(r'^operarios/$', views.WsOperarios.as_view(), name='operarios'),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -6,6 +6,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.views import logout
 from django.shortcuts import HttpResponse
 from django.views.generic import TemplateView
+from supra import views as supra
 
 
 # Create your views here.
@@ -28,4 +29,23 @@ class Logout(TemplateView):
         logout(request, **kwargs)
         return HttpResponse('{"res":"ok"}', content_type="application/json", status=201)
     # end def
+# end class
+
+
+class WsOperarios(supra.SupraListView):
+    model = models.Empleado
+    list_display = ['nombre', 'id']
+    paginate_by = 1000
+
+    class Renderer:
+        apellidos = 'last_name'
+    # end class
+
+    def nombre(self, obj, row):
+        return '%s %s' % (obj.first_name, obj.last_name)
+    # end def
+
+    def get_queryset(self):
+        queryset = super(WsOperarios, self).get_queryset()
+        return queryset
 # end class
