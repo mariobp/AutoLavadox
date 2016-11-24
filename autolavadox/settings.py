dfeb296f31ec739bd6aa6c1cb73c88ed05246354
mariobp <mariobarrpach@gmail.com>
2016-11-23 19:38:01 -0500

    humanize

--- autolavadox/settings.py
+++ autolavadox/settings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
     'django.contrib.staticfiles',
     'nested_admin',
     'django_select2',
