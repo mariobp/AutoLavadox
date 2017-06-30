f7c83b7ae6e3122a304780fc4a5c8e0b823f2885
darkdrei <darkdrei88@gmail.com>
2017-06-30 14:01:13 -0700

    agregar el modelo cuenta a estadistica  #2

--- estadistica/models.py
+++ estadistica/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 from operacion import models as operacion
 from django.db import models
+from subcripcion import models as suscripcion
 
 
 class TiemposOrden(models.Model):
     orden = models.ForeignKey(operacion.Orden)
     inicio = models.DateField()
     fin = models.DateField()
+    cuenta = models.ForeignKey(suscripcion.Cuenta)
 
     def __unicode__(self):
         print self.orden.vehiculo
