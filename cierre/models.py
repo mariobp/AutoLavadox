d76b66028b9f5033e66381f6a18abf2e563d9e3f
darkdrei <darkdrei88@gmail.com>
2017-07-08 07:22:15 -0500

    modificar el modelo de cierre #3

--- cierre/models.py
+++ cierre/models.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
-
+from subcripcion import models as suscripcion
 # Create your models here.
 
 class TipoServicio(models.Model):
     total = models.FloatField(default=0.0, null=True, blank=True)
-    comision = models.FloatField(default=0.0, null=True, blank=True) 
+    comision = models.FloatField(default=0.0, null=True, blank=True)
     inicio = models.DateField()
     fin = models.DateField()
+    cuenta = models.ForeignKey(suscripcion.Cuenta, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Cierre dia tipo de servicios'
@@ -18,10 +20,11 @@ class TipoServicio(models.Model):
 
 
 class Factura(models.Model):
-    total = models.FloatField(default=0.0, null=True, blank=True)
-    comision = models.FloatField(default=0.0, null=True, blank=True)
+    total = models.FloatField(default=0.0, null=True, blank=True,verbose_name='Total factura')
+    comision = models.FloatField(default=0.0, null=True, blank=True,verbose_name='Total comisión')
     inicio = models.DateField()
     fin = models.DateField()
+    cuenta = models.ForeignKey(suscripcion.Cuenta, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Cierre de factura'
