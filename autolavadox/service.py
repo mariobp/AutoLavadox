917e753177831bc3c580a35040be2de3c304cee7
darkdrei <darkdrei88@gmail.com>
2017-07-08 07:19:13 -0500

    Adicion de servicios para las funcionalidades de modificacion de admin y form  #4

--- autolavadox/service.py
+++ autolavadox/service.py
@@ -3,6 +3,7 @@
 from subcripcion import models as suscripcion
 from cuser.middleware import CuserMiddleware
 from django.db.models import Q
+from empleados import models as empleados
 
 class Service():
     instance = None
@@ -16,7 +17,37 @@ class Service():
     #end def
 
     @classmethod
-    def getCuenta(sef):
+    def isRecepcionista(self):
+        user = CuserMiddleware.get_user()
+        recepcionista = empleados.Recepcionista.objects.filter(id=user.id).first()
+        if recepcionista:
+            return True
+        #end if
+        return False
+    #end def
+
+    @classmethod
+    def isAdministrador(self):
+        user = CuserMiddleware.get_user()
+        administrador = empleados.Administrador.objects.filter(id=user.id).first()
+        if administrador:
+            return True
+        #end if
+        return False
+    #end def
+
+    @classmethod
+    def isCajero(self):
+        user = CuserMiddleware.get_user()
+        cajero = empleados.Cajero.objects.filter(id=user.id).first()
+        if cajero:
+            return True
+        #end if
+        return False
+    #end def
+
+    @classmethod
+    def getCuenta(self):
         user = CuserMiddleware.get_user()
         if user:
             id =CuserMiddleware.get_user().id
@@ -37,5 +68,17 @@ class Service():
             return r_cuenta,True,user.is_superuser
         #end if
         return False,False
+    #end def
+
+    @classmethod
+    def isUserCuenta(sef):
+        user = CuserMiddleware.get_user()
+        if user:
+            id =CuserMiddleware.get_user().id
+            cuenta = suscripcion.Cuenta.objects.filter(Q(cliente__id=id)|Q(persona__id=id)).first()
+            r_cuenta =  True if cuenta else False
+            return r_cuenta
+        #end if
+        return False
     #end def
 #end class
