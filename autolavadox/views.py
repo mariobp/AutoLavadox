663eeb0de22de12e3312b940e09ff5281ae6abd8
darkdrei <darkdrei88@gmail.com>
2017-07-08 07:19:22 -0500

    Adicion de servicios para las funcionalidades de modificacion de admin y form  #4

--- autolavadox/views.py
+++ autolavadox/views.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from autolavadox.service import Service
+from django.db.models import Q
+from supra import views as supra
 
 
 def set_queryset(query):
     ser = Service.get_instance()
     tem_cuenta,is_user,admin = ser.isUser()
-    print tem_cuenta,is_user,admin
-    print 'esta es una consulta  ',len(query)
     if tem_cuenta and is_user :
-        print 'Entro ', ser.getCuenta()
         cuenta = ser.getCuenta()
         query = query.filter(cuenta=cuenta)
-        for x in query:
-            print x.cuenta.id,'  ',cuenta.id
-        #end for
-        print len(query.filter(cuenta=cuenta))
     #end if
-    print 'esta es una consulta  ',len(query)
     return query
 #end def
 
@@ -28,3 +22,11 @@ class BaseAdmin(admin.ModelAdmin):
         return queryset.filter(Q(status=True)|Q(state=True)).order_by('-id')
     # end def
 #end class
+
+class BaseListSupra(supra.SupraListView):
+    def get_queryset(self):
+        queryset = super(BaseListSupra, self).get_queryset()
+        queryset = set_queryset(queryset)
+        return queryset
+    # end def
+# end class
