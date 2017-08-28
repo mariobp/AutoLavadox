b6a1e1609685db4f054f744c57f2cbcd986e5298
darkdrei <darkdrei88@gmail.com>
2017-08-28 18:35:02 -0500

    modificacion de url de cierre de factura

--- cierre/urls.py
+++ cierre/urls.py
@@ -4,6 +4,6 @@ import views
 
 # url ws de tipo de servicios porvehiculo
 urlpatterns = [
-    url(r'^factura/(?P<pk>\d+)/$', views.Factura.as_view(), name='factura'),
+    url(r'^factura/(?P<pk>\d+)/$', views.FacturaT.as_view(), name='facturat'),
     url(r'^factura/tipo/(?P<pk>\d+)/$', views.FacturaTipo.as_view(), name='factura'),
 ]
