2f4fadcb6d834cb227ff0cea4d65d42e69f477ba
darkdrei <darkdrei88@gmail.com>
2017-09-21 13:38:44 -0500

    listado de  todos los clientes

--- cliente/urls.py
+++ cliente/urls.py
@@ -5,6 +5,7 @@ import views
 urlpatterns = [
     url(r'^tipo/vehiculo/$', views.TiposVehiculos.as_view(), name='tipo_vehiculo'),
     url(r'^vehiculo/$', views.VehiculoInfo.as_view(), name='vehiculo'),
+    url(r'^vehiculo/list/$', views.VehiculoInfoList.as_view(), name='vehiculos'),
 ]
 
 
@@ -13,5 +14,6 @@ urlpatterns += [
     url(r'^add/vehiculo/$', views.VehiculoAdd.as_view(), name='add_vehiculo'),
     url(r'^edit/vehiculo/(?P<pk>\d+)/$', views.VehiculoEdit.as_view(), name='edit_vehiculo'),
     url(r'^add/cliente/inline/$', views.ClienteSupra.as_view(), name='cliente'),
-    url(r'^list/cliente/$', views.ClienteList.as_view(), name='list_cliente')
+    url(r'^list/cliente/$', views.ClienteList.as_view(), name='list_cliente'),
+    url(r'^list/cliente/all/$', views.ClienteListAll.as_view(), name='list_cliente_all'),
 ]
