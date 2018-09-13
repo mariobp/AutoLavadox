4f9981efc97b324509e50bf603e2f0e7fb131183
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-09-13 07:10:08 -0500

    update url setting

--- autolavadox/urls.py
+++ autolavadox/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     url(r'^cierre/', include('cierre.urls', namespace='cierre')),
     url(r'^favicon\.ico$', favicon_view),
 ]
-"""
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)"""
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
