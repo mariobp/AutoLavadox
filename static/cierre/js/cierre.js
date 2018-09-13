df38a943f1c3b0e03e27821f39083e0369984aab
darkdrei <eugenio.pajaro.ext@nokia.com>
2018-09-13 06:57:23 -0500

    update setting

--- static/cierre/js/cierre.js
+++
@@ -1,8 +0,0 @@
-$(document).on('ready',function(){
-    $('a.generar.addlink').on('click', function(){
-        return false;
-    });
-    $('a.generar.addlink').on('click', function(){
-        window.open($(this).attr('href'),'_blank');
-    });
-});
